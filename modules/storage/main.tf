5ea76d283b20178a60c5288bb587e3ade12952c9
Alok Kumar <maneeshksingh14@gmail.com>
2026-02-14 22:45:12 +0530

    add

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,10 +1,10 @@
-# resource "azurerm_storage_account" "stg3" {
-#   for_each = var.stgs
-#   name                     = each.value.name
-#   resource_group_name      = each.value.resource_group_name
-#   location                 = each.value.location
-#   account_tier             = each.value
-#   account_replication_type = each.value.account_replication_type
-# }
-# # account_tier             = "Standard"
-# #   account_replication_type = "GRS"
+resource "azurerm_storage_account" "stg3" {
+  for_each = var.stgs
+  name                     = each.value.name
+  resource_group_name      = each.value.resource_group_name
+  location                 = each.value.location
+  account_tier             = each.value
+  account_replication_type = each.value.account_replication_type
+}
+# account_tier             = "Standard"
+#   account_replication_type = "GRS"
--- modules/storage/variable.tf
+++ modules/storage/variable.tf
@@ -1,4 +1,3 @@
-# variable "stgs" {
+    variable "stgs" {}    
      
  
-# }
