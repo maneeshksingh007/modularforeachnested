f3bbc6ec085ead70bd4139deae7757f374772d3b
Alok Kumar <maneeshksingh14@gmail.com>
2026-02-14 23:47:07 +0530

    add

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "stg3" {
   name                     = each.value.name
   resource_group_name      = each.value.resource_group_name
   location                 = each.value.location
-  account_tier             = each.value
+  account_tier             = each.value.account_tier
   account_replication_type = each.value.account_replication_type
 }
 # account_tier             = "Standard"
